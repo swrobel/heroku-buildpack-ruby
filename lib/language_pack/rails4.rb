9f8db6fd88e461e9a1d2e50838e68015368012e0
Stefan Wrobel <swrobel@users.noreply.github.com>
2018-02-19 21:58:04 -0800

    Store/restore webpacker cache & public packs folders

--- lib/language_pack/rails4.rb
+++ lib/language_pack/rails4.rb
@@ -62,8 +62,16 @@ WARNING
     "public/assets"
   end
 
+  def public_packs_folder
+    "public/packs"
+  end
+
   def default_assets_cache
     "tmp/cache/assets"
+  end
+
+  def default_webpacker_cache
+    "tmp/cache/webpacker"
   end
 
   def run_assets_precompile_rake_task
@@ -80,7 +88,9 @@ WARNING
         topic("Preparing app for Rails asset pipeline")
 
         @cache.load_without_overwrite public_assets_folder
+        @cache.load_without_overwrite public_packs_folder
         @cache.load default_assets_cache
+        @cache.load default_webpacker_cache
 
         precompile.invoke(env: rake_env)
 
@@ -93,7 +103,10 @@ WARNING
 
           cleanup_assets_cache
           @cache.store public_assets_folder
+          cleanup_webpacker_cache
+          @cache.store public_packs_folder
           @cache.store default_assets_cache
+          @cache.store default_webpacker_cache
         else
           precompile_fail(precompile.output)
         end
@@ -104,6 +117,12 @@ WARNING
   def cleanup_assets_cache
     instrument "rails4.cleanup_assets_cache" do
       LanguagePack::Helpers::StaleFileCleaner.new(default_assets_cache).clean_over(ASSETS_CACHE_LIMIT)
+    end
+  end
+
+  def cleanup_webpacker_cache
+    instrument "rails4.cleanup_webpacker_cache" do
+      LanguagePack::Helpers::StaleFileCleaner.new(default_webpacker_cache).clean_over(ASSETS_CACHE_LIMIT)
     end
   end
 end
